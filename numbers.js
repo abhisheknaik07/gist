a7ca1f4b5b480bdcc6ece13e05019bc5413f80bb
abhisheknaik07 <34272484+abhisheknaik07@users.noreply.github.com>
2017-12-05 17:54:07 +0530

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -2,11 +2,11 @@ function PrintNumbers()
 {
         for(i=1;i<=100;i++)
         {
-                if(i%3==0)
+                if(i%3===0)
                         Console.Log("Earth")
-                 if(i%5==0)
+                 if(i%5===0)
                         Console.Log("Jupiter")        
-                if(i%9==0)
+                if(i%9===0)
                         Console.Log("Pluto")                
                 Console.Log(i)
 
