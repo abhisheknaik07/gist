e1d8397161470abab302a2603bd09cecd5010275
abhisheknaik07 <34272484+abhisheknaik07@users.noreply.github.com>
2017-12-05 17:44:26 +0530

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,15 @@
+function PrintNumbers()
+{
+        for(i=1;i<=100;i++)
+        {
+                if(i%3==0)
+                        Console.Log("Earth")
+                 if(i%5==0)
+                        Console.Log("Jupiter")        
+                if(i%9==0)
+                        Console.Log("Pluto")                
+                Console.Log(i)
+
+        }
+
+} 
