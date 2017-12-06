aec6847f425a3d7a682092ac9c631dc6e29a8930
abhisheknaik07 <34272484+abhisheknaik07@users.noreply.github.com>
2017-12-06 09:33:04 +0530

    Numbers.js
    For every multiple of 3, print "Earth". For every multiple of 5, print "Jupiter". For every multiple of 9, print "Pluto".

--- numbers.js
+++ numbers.js
@@ -1,15 +1,19 @@
-function PrintNumbers()
-{
-        for(i=1;i<=100;i++)
-        {
-                if(i%3===0)
-                        Console.Log("Earth")
-                 if(i%5===0)
-                        Console.Log("Jupiter")        
-                if(i%9===0)
-                        Console.Log("Pluto")                
-                Console.Log(i)
+/* Write a program in JS that prints numbers 1 to 100.
+For every multiple of 3, print "Earth". For every multiple of 5, print "Jupiter". For every multiple of 9, print "Pluto". */
 
-        }
-
+function PrintNumbers(num)
+        {        
+        if(i%3===0)
+                return "Earth";
+         if(i%5===0)
+                return "Jupiter";    
+        if(i%9===0)
+                return "Pluto";              
+         return i;
+      
 } 
+for(i=1;i<=100;i++)
+{
+console.log(PrintNumbers(i));
+}
+
